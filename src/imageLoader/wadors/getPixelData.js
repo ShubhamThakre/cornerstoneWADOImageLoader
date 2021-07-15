db67ce1b54db669ddab99aebadd838ce467eec15
ShubhamThakre <shhubhamthakceray@gmail.com>
2021-07-15 11:12:52 +0530

    add data to cache

--- src/imageLoader/wadors/getPixelData.js
+++ src/imageLoader/wadors/getPixelData.js
@@ -31,6 +31,63 @@ function uint8ArrayToString(data, offset, length) {
   return str;
 }
 
+function loadPixelData(imageFrameAsArrayBuffer) {
+  // request succeeded, Parse the multi-part mime response
+  const response = new Uint8Array(imageFrameAsArrayBuffer);
+
+  // First look for the multipart mime header
+  const tokenIndex = findIndexOfString(response, '\r\n\r\n');
+
+
+  if (tokenIndex === -1) {
+    reject(new Error('invalid response - no multipart mime header'));
+  }
+  const header = uint8ArrayToString(response, 0, tokenIndex);
+  // Now find the boundary  marker
+  const split = header.split('\r\n');
+  const boundary = findBoundary(split);
+
+  if (!boundary) {
+    reject(new Error('invalid response - no boundary marker'));
+  }
+  const offset = tokenIndex + 4; // skip over the \r\n\r\n
+
+  // find the terminal boundary marker
+  const endIndex = findIndexOfString(response, boundary, offset);
+
+  if (endIndex === -1) {
+    reject(new Error('invalid response - terminating boundary not found'));
+  }
+
+  // Remove \r\n from the length
+  const length = endIndex - offset - 2;
+
+  //addding comment
+  console.log('shubham', {
+    contentType: findContentType(split),
+    imageFrame: {
+      pixelData: new Uint8Array(imageFrameAsArrayBuffer, offset, length),
+    },
+  }, 'uri', uri, 'imageId', imageId, 'headers', headers, 'imageFrameAsArrayBuffer', imageFrameAsArrayBuffer, typeof (imageFrameAsArrayBuffer));
+
+  const options = {
+    headers: {
+      'Content-Type': 'application/octet-stream'
+    }
+  }
+
+  // adding the arrayBUffer data to cache
+  const jsonRes = new Response(imageFrameAsArrayBuffer, options)
+  cache.put(uri, jsonRes)
+
+  return {
+    contentType: findContentType(split),
+    imageFrame: {
+      pixelData: new Uint8Array(imageFrameAsArrayBuffer, offset, length),
+    },
+  }
+}
+
 async function getPixelData(uri, imageId, mediaType = 'application/octet-stream') {
   const headers = {
     accept: mediaType,
@@ -46,64 +103,11 @@ async function getPixelData(uri, imageId, mediaType = 'application/octet-stream'
     const loadPromise = xhrRequest(uri, imageId, headers);
 
     loadPromise.then(function (imageFrameAsArrayBuffer /* , xhr*/) {
-      // request succeeded, Parse the multi-part mime response
-      const response = new Uint8Array(imageFrameAsArrayBuffer);
-
-      // First look for the multipart mime header
-      const tokenIndex = findIndexOfString(response, '\r\n\r\n');
-
-
-      if (tokenIndex === -1) {
-        reject(new Error('invalid response - no multipart mime header'));
-      }
-      const header = uint8ArrayToString(response, 0, tokenIndex);
-      // Now find the boundary  marker
-      const split = header.split('\r\n');
-      const boundary = findBoundary(split);
-
-      if (!boundary) {
-        reject(new Error('invalid response - no boundary marker'));
-      }
-      const offset = tokenIndex + 4; // skip over the \r\n\r\n
-
-      // find the terminal boundary marker
-      const endIndex = findIndexOfString(response, boundary, offset);
-
-      if (endIndex === -1) {
-        reject(new Error('invalid response - terminating boundary not found'));
-      }
-
-      // Remove \r\n from the length
-      const length = endIndex - offset - 2;
-
-      //addding comment
-      console.log('shubham', {
-        contentType: findContentType(split),
-        imageFrame: {
-          pixelData: new Uint8Array(imageFrameAsArrayBuffer, offset, length),
-        },
-      }, 'uri', uri, 'imageId', imageId, 'headers', headers, 'imageFrameAsArrayBuffer', imageFrameAsArrayBuffer, typeof (imageFrameAsArrayBuffer));
-
-      const options = {
-        headers: {
-          'Content-Type': 'application/octet-stream'
-        }
-      }
-
-      // adding the arrayBUffer data to cache
-      // const buffer = new ArrayBuffer(imageFrameAsArrayBuffer)
-      const jsonRes = new Response(imageFrameAsArrayBuffer, options)
-      cache.put(uri, jsonRes)
-
-
+      // load the pixel data from loadPixelData function
+      const pixelData = loadPixelData(imageFrameAsArrayBuffer)
 
       // return the info for this pixel data
-      resolve({
-        contentType: findContentType(split),
-        imageFrame: {
-          pixelData: new Uint8Array(imageFrameAsArrayBuffer, offset, length),
-        },
-      });
+      resolve(pixelData);
     }, reject);
   });
 }
