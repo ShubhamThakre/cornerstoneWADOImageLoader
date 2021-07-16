ac9a43472f3b2f69f4060d6592a39117cc4810a7
ShubhamThakre <shhubhamthakceray@gmail.com>
2021-07-16 12:37:21 +0530

    removed the console logs

--- src/imageLoader/wadors/getPixelData.js
+++ src/imageLoader/wadors/getPixelData.js
@@ -93,20 +93,10 @@ async function getPixelData(uri, imageId, mediaType = 'application/octet-stream'
   };
   const cache = await caches.open('my-cache');
 
-  // caches.open('my-cache')
-  //   .then(cache => {
-  //     cache.match('https://api-qa.quantx.cloud/studies/2.16.840.1.113786.1.982.8.706647296.796/series/1.3.12.2.1107.5.2.33.37426.30000015070219112348400000161/instance/1.3.12.2.1107.5.2.33.37426.30000015070219112348400000163/frames/1')
-  //       .then(res =>{
-  //         console.log('my response',res.arrayBuffer().then(a => console.log('aa', a, new Uint8Array(a))))
-  //       })
-  //   })
-
   const cacheData = await cache.match(uri);
-  console.log(`cacheData`, cacheData)
 
   return new Promise(async (resolve, reject) => {
     if (cacheData) {
-      console.log('inside if');
       cacheData.arrayBuffer().then(buffer => {
         console.log(buffer)
         // load the pixel data from loadPixelData function
@@ -116,7 +106,6 @@ async function getPixelData(uri, imageId, mediaType = 'application/octet-stream'
         resolve(pixelData);
       }, reject);
     } else {
-      console.log('outside if');
       const loadPromise = xhrRequest(uri, imageId, headers);
 
       loadPromise.then(function (imageFrameAsArrayBuffer /* , xhr*/) {
